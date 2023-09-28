6a0cbd4db0aa05061f11e6b06ed6a3a57cd164a9
kezia2909 <c14190048@john.petra.ac.id>
2023-09-28 23:43:06 +0700

    fix error -> change item model

--- lib/src/models/item_model.dart
+++ lib/src/models/item_model.dart
@@ -1,24 +1,41 @@
 class ItemModel {
+  int? _totalPages;
   int? _page;
   int? _totalResults;
-  int? _totalPages;
-  List<_ResultModel>? _results = [];
+  List<_ResultModel> _results = [];
 
-  ItemModel.fromJson(Map<String, dynamic> parsedJson) {
-    _page = parsedJson['page'];
-    _totalResults = parsedJson['total_results'];
-    _totalPages = parsedJson['total_pages'];
-    List<_ResultModel> temp = [];
-    for (int i = 0; i < parsedJson['results'].length; i++) {
-      _ResultModel result = _ResultModel.fromJson(parsedJson['results'][i]);
-      temp.add(result);
-    }
-    _results = temp;
+  List<_ResultModel> get results => _results;
+
+  ItemModel(
+      {required totalPages,
+      required page,
+      required totalResults,
+      required results}) {
+    this._totalPages = totalPages;
+    this._totalResults = totalResults;
+    this._page = page;
+    this._results = results;
   }
 
-  List<_ResultModel>? get results => _results;
-
-  get length => null;
+  ItemModel.fromJson(Map<String, dynamic> parsedJson) {
+    _totalPages = parsedJson['total_pages'];
+    _totalResults = parsedJson['total_results'];
+    _page = parsedJson['page'];
+    if (parsedJson['results'] != null) {
+      _results = <_ResultModel>[];
+      (parsedJson['results'] as List).forEach(
+        (element) {
+          _results.add(_ResultModel.fromJson(element));
+        },
+      );
+    }
+    // List<_ResultModel> temp = [];
+    // for (int i = 0; i < parsedJson['results'].length; i++) {
+    //   _ResultModel result = _ResultModel.fromJson(parsedJson['results'][i]);
+    //   temp.add(result);
+    // }
+    // _results = temp;
+  }
 }
 
 class _ResultModel {
@@ -42,9 +59,17 @@ class _ResultModel {
     _title = result['title'];
     _adult = result['adult'] ? false : result['adult'];
     _backdropPath = result['backdrop_path'];
-    for (int i = 0; i < result['genre_ids'].length; i++) {
-      _genreIds!.add(result['genre_ids'][i]);
+    if (result['genre_ids'] != null) {
+      _genreIds = <int>[];
+      (result['genre_ids'] as List).forEach(
+        (element) {
+          _genreIds?.add(element);
+        },
+      );
     }
+    // for (int i = 0; i < result['genre_ids'].length; i++) {
+    //   _genreIds!.add(result['genre_ids'][i]);
+    // }
     _originalLanguage = result['original_language'];
     _originalTitle = result['original_title'];
     _overview = result['overview'];
